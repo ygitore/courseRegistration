332564fac474e8e55c7aad5fa9db43dcbb1898f0
yitbarekgitore <yitbareky@gmail.com>
2020-10-22 10:18:35 -0500

    created database, models and repository

---
+++ CourseRegistration/Models/Course.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CourseRegistration.Model
+{
+    public class Course
+    {
+        public int Id { get; set; }
+        public string CourseName { get; set; }
+        public string CourseCode { get; set; }
+    }
+}
---
+++ CourseRegistration/Models/Student.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CourseRegistration.Model
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public int GenderId { get; set; }
+        public Gender Gender { get; set; }
+        public string Email { get; set; }
+        public int DepartmentId { get; set; }
+        public Department Department { get; set; }
+        public int CourseId { get; set; }
+        public Course Course { get; set; }
+        public int InstructorId { get; set; }
+        public Instructor Instructor { get; set; }
+    }
+}
