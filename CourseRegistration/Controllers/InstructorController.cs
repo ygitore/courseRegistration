8f0d29ef72795b98b5e35a43ca8364a9cfb4aa0e
yitbarekgitore <yitbareky@gmail.com>
2020-10-26 05:56:47 -0500

    getting all instructor record, adding new instructor record to database and getting single instructor record

--- CourseRegistration/Controllers/InstructorController.cs
+++ CourseRegistration/Controllers/InstructorController.cs
@@ -2,8 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CourseRegistration.Model;
+using CourseRegistration.Repositories;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 
 namespace CourseRegistration.Controllers
 {
@@ -11,5 +14,35 @@ namespace CourseRegistration.Controllers
     [ApiController]
     public class InstructorController : ControllerBase
     {
+        public readonly InstructorRepository _instructorRepository;
+        public InstructorController(IConfiguration config)
+        {
+            _instructorRepository = new InstructorRepository(config);
+        }
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_instructorRepository.GetAll());
+        }
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            Instructor instructor = _instructorRepository.Get(id);
+            if (instructor == null)
+            {
+                return BadRequest();
+            }
+            return Ok(instructor);
+        }
+        [HttpPost]
+        public IActionResult Post(Instructor instructor)
+        {
+            if (instructor == null)
+            {
+                return BadRequest();
+            }
+            _instructorRepository.Add(instructor);
+            return Ok(instructor);
+        }
     }
 }
--- CourseRegistration/Repositories/InstructorRepository.cs
+++ CourseRegistration/Repositories/InstructorRepository.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using CourseRegistration.Model;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +10,108 @@ namespace CourseRegistration.Repositories
 {
     public class InstructorRepository
     {
+        public readonly IConfiguration _config;
+        public InstructorRepository(IConfiguration config)
+        {
+            _config = config;
+        }
+        public SqlConnection connection
+        {
+            get
+            {
+                return new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            }
+        }
+        public List<Instructor> GetAll()
+        {
+            using (var conn = connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = "SELECT Id, firstName, lastName, age, genderId, email,departmentId from Instructor";
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    var instructors = new List<Instructor>();
+                    while (reader.Read())
+                    {
+                        Instructor instructor = new Instructor
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            FirstName = reader.GetString(reader.GetOrdinal("firstName")),
+                            LastName = reader.GetString(reader.GetOrdinal("lastName")),
+                            Email = reader.GetString(reader.GetOrdinal("email")),
+                            Age = reader.GetInt32(reader.GetOrdinal("age")),
+                            GenderId = reader.GetInt32(reader.GetOrdinal("genderId")),
+                            DepartmentId = reader.GetInt32(reader.GetOrdinal("genderId")),
+                        };
+                        instructors.Add(instructor);
+                    }
+                    reader.Close();
+                    return instructors;
+                };
+            }
+        }
+        public Instructor Get(int id)
+        {
+            using (var conn = connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = "SELECT Id, firstName, lastName, age, genderId, email,departmentId from Instructor where Id = @id";
+                    cmd.Parameters.AddWithValue("@id", id);
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    Instructor instructor = null;
+                    while (reader.Read())
+                    {
+                        instructor = new Instructor
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            FirstName = reader.GetString(reader.GetOrdinal("firstName")),
+                            LastName = reader.GetString(reader.GetOrdinal("lastName")),
+                            Email = reader.GetString(reader.GetOrdinal("email")),
+                            Age = reader.GetInt32(reader.GetOrdinal("age")),
+                            GenderId = reader.GetInt32(reader.GetOrdinal("genderId")),
+                            DepartmentId = reader.GetInt32(reader.GetOrdinal("genderId")),                        };
+                    }
+                    reader.Close();
+                    return instructor;
+                };
+            };
+        }
+        public void Add(Instructor instructor)
+        {
+            using (var conn = connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = "Insert Into Instructor (firstName, lastName, age, genderId, email,departmentId) OUTPUT INSERTED.ID Values(@fName,@lName, @age,@genderid, @email, @departmentId) ";
+                    cmd.Parameters.AddWithValue("@fName", instructor.FirstName);
+                    cmd.Parameters.AddWithValue("@lName", instructor.LastName);
+                    cmd.Parameters.AddWithValue("@age", instructor.Age);
+                    cmd.Parameters.AddWithValue("@email", instructor.Email);
+                    if (instructor.GenderId.ToString() == null)
+                    {
+                        cmd.Parameters.AddWithValue("@genderId", DBNull.Value);
+                    }
+                    else
+                    {
+                        cmd.Parameters.AddWithValue("@genderId", instructor.GenderId);
+                    }
+
+                    if (instructor.DepartmentId.ToString() == null)
+                    {
+                        cmd.Parameters.AddWithValue("@departmentId", DBNull.Value);
+                    }
+                    else
+                    {
+                        cmd.Parameters.AddWithValue("@departmentId", instructor.DepartmentId);
+                    }                    
+
+                    instructor.Id = (int)cmd.ExecuteScalar();
+                }
+            }
+        }
     }
 }
